aafb355228100109d6bff2256a9d1051333afcd6
VladimirShat <vovahat00@gmail.com>
2021-07-25 00:31:20 +0500

    Added screen size independence

--- Screen Particles/Assets/ScreenParticles.cs
+++ Screen Particles/Assets/ScreenParticles.cs
@@ -5,30 +5,40 @@ using System;
 
 public class ScreenParticles : MonoBehaviour
 {
+    #region Public Variables
     [Serializable]
     public enum MovingMode { BlackHole, UpRight, RandomDirection }
 
     [Space(5)]
     public Camera gameCamera;
-    public RenderTexture cameraOutputRT;
-    public MovingMode movingMode = MovingMode.BlackHole;
-    [Range(-1,1)]
-    public float particlesSpeed;
+    public Camera RTCamera;
 
     [Space(5)]
-    [Header("Screen Particles")]
+    [Header("Particles Settings")]
     public string particlesLayerName;
-    public int rowParticlesCount = 60;
-    public int columnParticlesCount = 60;
+    public int columnsCount = 60;
+    public int rowsCount = 60;
     public Mesh particleMesh;
     public float particleScale = 0.035f;
+    [Space(5)]
+    [Header("Particles Movement")]
+    [Range(-1, 1)]
+    public float particlesSpeed;
+    public MovingMode movingMode = MovingMode.BlackHole;
+    #endregion
 
+    private RenderTexture cameraOutputRT;
     private List<ScreenParticle> particles = new List<ScreenParticle>();
     private bool isMoving = false;
 
+    void Awake()
+    {
+        cameraOutputRT = new RenderTexture(Screen.width, Screen.height, 24);
+        RTCamera.targetTexture = cameraOutputRT;
+    }
+
     void Update()
     {
-        //init particles
         if (Input.GetKeyDown(KeyCode.Space))
         {
             RenderTexture.active = cameraOutputRT;
@@ -36,27 +46,31 @@ public class ScreenParticles : MonoBehaviour
             Texture2D texture2D = new Texture2D(cameraOutputRT.width, cameraOutputRT.height);
             texture2D.ReadPixels(new Rect(0, 0, cameraOutputRT.width, cameraOutputRT.height), 0, 0);
             texture2D.Apply();
-
+            int i = 0;
             var rnd = new System.Random((int)Time.time);
-            for (int x = 0; x <= texture2D.width; x += texture2D.width / rowParticlesCount)
+            for (int x = 0; x < texture2D.width; x += texture2D.width / columnsCount)
             {
-                for (int y = 0; y <= texture2D.height; y += texture2D.height / columnParticlesCount)
+                for (int y = 0; y < texture2D.height; y += texture2D.height / rowsCount)
                 {
                     Ray ray = gameCamera.ScreenPointToRay(new Vector3(x, y, 0));
                     RaycastHit hitInfo;
                     Physics.Raycast(ray, out hitInfo);
                     Color color = texture2D.GetPixel(x, y);
 
-                    particles.Add(new ScreenParticle(CreateParticle("particle", color, hitInfo.point), movingMode, rnd));
+                    particles.Add(new ScreenParticle(CreateParticle("particle" + i, color, hitInfo), movingMode, rnd));
+                    i++;
                 }
             }
 
-            //on screen stay only particles
             gameCamera.clearFlags = CameraClearFlags.SolidColor;
+            //now camera see just particles layer
             gameCamera.cullingMask = 1 << LayerMask.NameToLayer(particlesLayerName);
             isMoving = true;
         }
+    }
 
+    private void FixedUpdate()
+    {
         if (isMoving)
         {
             for (int i = 0; i < particles.Count; i++)
@@ -74,16 +88,17 @@ public class ScreenParticles : MonoBehaviour
         }
     }
 
-    private GameObject CreateParticle(string name, Color color, Vector3 position)
+    private GameObject CreateParticle(string name, Color color, RaycastHit hitInfo)
     {
         GameObject particle = new GameObject(name, typeof(MeshFilter), typeof(MeshRenderer));
+        particle.transform.SetParent(hitInfo.collider.gameObject.GetComponent<Transform>());
         particle.GetComponent<MeshFilter>().mesh = particleMesh;
         particle.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Unlit/Color"));
         particle.GetComponent<MeshRenderer>().sharedMaterial.color = color;
         particle.layer = LayerMask.NameToLayer(particlesLayerName);
         particle.transform.localScale = Vector3.one * particleScale;
-
-        particle.transform.position = position;
+        particle.transform.rotation = Quaternion.FromToRotation(Vector3.forward, -hitInfo.normal);
+        particle.transform.position = hitInfo.point;
         return particle;
     }
 
@@ -101,7 +116,7 @@ public class ScreenParticles : MonoBehaviour
             {
                 case MovingMode.BlackHole:
                     {
-                        var rand = new System.Random((int)Time.time);
+                        var rand = new System.Random((int)UnityEngine.Random.Range(float.MinValue, float.MaxValue));
                         this.target = new Vector3((float)rand.NextDouble(), (float)rand.NextDouble(), this.particle.transform.position.z);
                     }
                     break;
@@ -117,7 +132,7 @@ public class ScreenParticles : MonoBehaviour
                     }
                     break;
                 default:
-                    throw new Exception("Unknown MovingMode. Please add new mode to enum");
+                    throw new Exception("Unknown MovingMode. Please add new case for this mode");
             }
 
             this.originPos = particle.transform.position;
